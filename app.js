22a48ce669e38a8e37dab99659c285a822f7d169
JackAlmutairi <39318606+JackAlmutairi@users.noreply.github.com>
2026-02-12 20:39:55 -0800

    added a folder structure for views and edited app.js

--- app.js
+++ app.js
@@ -5,7 +5,28 @@
 // Express
 const express = require('express');  // We are using the express library for the web server
 const app = express();               // We need to instantiate an express object to interact with the server in our code
-const PORT = 65571;     // Set a port number
+const PORT = process.env.PORT || 65571;     // Set a port number
+
+app.set('view engine', 'ejs');
+app.use(express.static(public));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/users', async (req, res) => {
+try {
+    const query1 = await db.query('SELECT * FROM Users ')
+}
+})
 
 // Database 
 const db = require('./db-connector');
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://classwork.engr.oregonstate.edu:${PORT}`);
+});
