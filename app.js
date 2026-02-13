7d99d59dfc03253c87cffed5517c9a6fb702430b
Ian Hale <133537927+Halei-6103@users.noreply.github.com>
2026-02-12 22:07:04 -0800

    developed the base frontend UI for review store

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 /*
     SETUP
 */
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 35571;
 
-// Express
-const express = require('express');  // We are using the express library for the web server
-const app = express();               // We need to instantiate an express object to interact with the server in our code
-const PORT = 65571;     // Set a port number
-
-// Database 
 const db = require('./db-connector');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+
+/*
+    ROUTES
+*/
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/developers', (req, res) => res.render('developers/index'));
+app.get('/developers/browse', (req, res) => res.render('developers/browse', { title: 'Browse Developers', developers: [] }));
+app.get('/developers/add', (req, res) => res.render('developers/add', { title: 'Add Developer' }));
+app.get('/developers/update', (req, res) => res.render('developers/update'));
+app.get('/developers/delete', (req, res) => res.render('developers/delete'));
+
+app.get('/games', (req, res) => res.render('games/index'));
+app.get('/games/browse', (req, res) => res.render('games/browse', { title: 'Browse Games', games: [] }));
+app.get('/games/add', (req, res) => res.render('games/add', { title: 'Add Game', developers: [] }));
+app.get('/games/update', (req, res) => res.render('games/update'));
+app.get('/games/delete', (req, res) => res.render('games/delete'));
+
+app.get('/users', (req, res) => res.render('users/index'));
+app.get('/users/browse', (req, res) => res.render('users/browse', { title: 'Browse Users', users: [] }));
+app.get('/users/add', (req, res) => res.render('users/add', { title: 'Add User' }));
+app.get('/users/update', (req, res) => res.render('users/update'));
+app.get('/users/delete', (req, res) => res.render('users/delete'));
+
+app.get('/purchases', (req, res) => res.render('purchases/index'));
+app.get('/purchases/browse', (req, res) => res.render('purchases/browse', { title: 'Browse Purchases', purchases: [] }));
+app.get('/purchases/add', (req, res) => res.render('purchases/add', { title: 'Add Purchase', users: [], games: [] }));
+app.get('/purchases/update', (req, res) => res.render('purchases/update'));
+app.get('/purchases/delete', (req, res) => res.render('purchases/delete'));
+
+app.get('/reviews', (req, res) => res.render('reviews/index'));
+app.get('/reviews/browse', (req, res) => res.render('reviews/browse', { title: 'Browse Reviews', reviews: [] }));
+app.get('/reviews/add', (req, res) => res.render('reviews/add', { title: 'Add Review', users: [], games: [] }));
+app.get('/reviews/update', (req, res) => res.render('reviews/update'));
+app.get('/reviews/delete', (req, res) => res.render('reviews/delete'));
+
+app.post('/developers/add', (req, res) => res.redirect('/developers'));
+app.post('/games/add', (req, res) => res.redirect('/games'));
+app.post('/users/add', (req, res) => res.redirect('/users'));
+app.post('/purchases/add', (req, res) => res.redirect('/purchases'));
+app.post('/reviews/add', (req, res) => res.redirect('/reviews'));
+
+/*
+    LISTENER
+*/
+app.listen(PORT, () => {
+  console.log('Server running on port %s', PORT);
+});
---
+++ db-connector.js
@@ -0,0 +1,8 @@
+/*
+  Database connection. Replace with real mysql2 pool when credentials are set.
+  Example:
+    const mysql = require('mysql2/promise');
+    const pool = mysql.createPool({ host: '...', user: '...', password: '...', database: '...' });
+    module.exports = pool;
+*/
+module.exports = {};
