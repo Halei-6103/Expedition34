fe5bfff1dd61e55abb51f24ad862bd2239d5bc7d
JackAlmutairi <39318606+JackAlmutairi@users.noreply.github.com>
2026-02-12 21:37:38 -0800

    did a test site for users and it works

--- app.js
+++ app.js
@@ -3,17 +3,19 @@
 */
 
 // Express
+require('dotenv').config();
 const express = require('express');  // We are using the express library for the web server
 const app = express();               // We need to instantiate an express object to interact with the server in our code
-const PORT = process.env.PORT || 56562;     // Set a port number
+const PORT = process.env.PORT || 8000;     // Set a port number
+const db = require('./db-connector');
 
-require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
 
 
-app.get('/', (req, res) => {
+app.get('/', async function (req, res) {
+
     res.render('index');
 });
 
@@ -21,17 +23,17 @@ app.get('/users', async (req, res) => {
 try {
     const query1 = 'SELECT * FROM Users; ';
     const [usersRow] = await db.query(query1);
+
+    res.render('users', { users: usersRow})
 } catch (error) {
     console.error('Error executing queries:', error);
     res.status(500).send(
         'An error occured while executing the database queries'
     );
   }
-}
-);
+});
 
 // Database 
-const db = require('./db-connector');
 
 
 
