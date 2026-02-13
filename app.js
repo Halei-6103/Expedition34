691580b347a71169ddcc97e19239311ec6ab9bf0
JackAlmutairi <39318606+JackAlmutairi@users.noreply.github.com>
2026-02-12 20:52:41 -0800

    removed some unneeded folders and added an ignore for password stuff

--- app.js
+++ app.js
@@ -5,12 +5,11 @@
 // Express
 const express = require('express');  // We are using the express library for the web server
 const app = express();               // We need to instantiate an express object to interact with the server in our code
-const PORT = process.env.PORT || 65571;     // Set a port number
+const PORT = process.env.PORT || 56562;     // Set a port number
 
 app.set('view engine', 'ejs');
-app.use(express.static(public));
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -18,9 +17,16 @@ app.get('/', (req, res) => {
 
 app.get('/users', async (req, res) => {
 try {
-    const query1 = await db.query('SELECT * FROM Users ')
+    const query1 = 'SELECT * FROM Users; ';
+    const [usersRow] = await db.query(query1);
+} catch (error) {
+    console.error('Error executing queries:', error);
+    res.status(500).send(
+        'An error occured while executing the database queries'
+    );
+  }
 }
-})
+);
 
 // Database 
 const db = require('./db-connector');
