32d3fb01e1a5c7d758a318300301ff6606e39625
JackAlmutairi <39318606+JackAlmutairi@users.noreply.github.com>
2026-02-12 23:25:04 -0800

    connected Database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /*
     SETUP
 */
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -17,32 +19,103 @@ app.use(express.urlencoded({ extended: true }));
 */
 app.get('/', (req, res) => res.render('index'));
 
+
 app.get('/developers', (req, res) => res.render('developers/index'));
-app.get('/developers/browse', (req, res) => res.render('developers/browse', { title: 'Browse Developers', developers: [] }));
+app.get('/developers/browse', async function (req, res) {
+  try {
+    const query1 = 'SELECT * FROM Developers;';
+    const [rows] = await db.query(query1);
+
+    res.render('developers/browse', {
+      title: 'Browse Developers',
+      developers: rows
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
 app.get('/developers/add', (req, res) => res.render('developers/add', { title: 'Add Developer' }));
 app.get('/developers/update', (req, res) => res.render('developers/update'));
 app.get('/developers/delete', (req, res) => res.render('developers/delete'));
 
 app.get('/games', (req, res) => res.render('games/index'));
-app.get('/games/browse', (req, res) => res.render('games/browse', { title: 'Browse Games', games: [] }));
+app.get('/games/browse', async function (req, res) {
+  try {
+    const query1 = 'SELECT * FROM Games;';
+    const [games] = await db.query(query1);
+
+    res.render('games/browse', {
+      title: 'Browse Games',
+      games: games
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
 app.get('/games/add', (req, res) => res.render('games/add', { title: 'Add Game', developers: [] }));
 app.get('/games/update', (req, res) => res.render('games/update'));
 app.get('/games/delete', (req, res) => res.render('games/delete'));
 
 app.get('/users', (req, res) => res.render('users/index'));
-app.get('/users/browse', (req, res) => res.render('users/browse', { title: 'Browse Users', users: [] }));
+app.get('/users/browse', async function (req, res) {
+  try {
+    const query1 = 'SELECT * FROM Users;';
+    const [users] = await db.query(query1);
+
+    res.render('users/browse', {
+      title: 'Browse Users',
+      users: users
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
 app.get('/users/add', (req, res) => res.render('users/add', { title: 'Add User' }));
 app.get('/users/update', (req, res) => res.render('users/update'));
 app.get('/users/delete', (req, res) => res.render('users/delete'));
 
 app.get('/purchases', (req, res) => res.render('purchases/index'));
-app.get('/purchases/browse', (req, res) => res.render('purchases/browse', { title: 'Browse Purchases', purchases: [] }));
+app.get('/purchases/browse', async function (req, res) {
+  try {
+    const query1 = 'SELECT * FROM Purchases;';
+    const [purchases] = await db.query(query1);
+
+    res.render('purchases/browse', {
+      title: 'Browse Purchases',
+      purchases: purchases
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
 app.get('/purchases/add', (req, res) => res.render('purchases/add', { title: 'Add Purchase', users: [], games: [] }));
 app.get('/purchases/update', (req, res) => res.render('purchases/update'));
 app.get('/purchases/delete', (req, res) => res.render('purchases/delete'));
 
 app.get('/reviews', (req, res) => res.render('reviews/index'));
-app.get('/reviews/browse', (req, res) => res.render('reviews/browse', { title: 'Browse Reviews', reviews: [] }));
+app.get('/reviews/browse', async function (req, res) {
+  try {
+    const query1 = 'SELECT * FROM Reviews;';
+    const [reviews] = await db.query(query1);
+
+    res.render('reviews/browse', {
+      title: 'Browse Reviews',
+      reviews: reviews
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
+});
 app.get('/reviews/add', (req, res) => res.render('reviews/add', { title: 'Add Review', users: [], games: [] }));
 app.get('/reviews/update', (req, res) => res.render('reviews/update'));
 app.get('/reviews/delete', (req, res) => res.render('reviews/delete'));
--- db-connector.js
+++ db-connector.js
@@ -1,8 +1,12 @@
-/*
-  Database connection. Replace with real mysql2 pool when credentials are set.
-  Example:
-    const mysql = require('mysql2/promise');
-    const pool = mysql.createPool({ host: '...', user: '...', password: '...', database: '...' });
-    module.exports = pool;
-*/
-module.exports = {};
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10
+}).promise();
+
+module.exports = pool;
